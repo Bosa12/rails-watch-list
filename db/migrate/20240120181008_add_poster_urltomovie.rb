43d6b89ec06b2d2d0f8c44b756b53668c1ebb5a6
catriona beamish <riccibozza@gmail.com>
2024-01-23 21:06:49 +0000

    my movie

---
+++ db/migrate/20240120181008_add_poster_urltomovie.rb
@@ -0,0 +1,6 @@
+class AddPosterUrltomovie < ActiveRecord::Migration[7.1]
+  def change
+    add_column :movies, :poster_url, :string
+    add_column :movies, :rating, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_20_171546) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_20_181008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_20_171546) do
     t.text "overview"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "poster_url"
+    t.float "rating"
   end
 
   add_foreign_key "bookmarks", "lists"
